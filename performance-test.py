a15a5831e9b60cc5249eba51d5619b2bbf2c77e0
k.chiyo <k.chiyo@yamawa.com>
2025-03-21 10:24:12 +0900

    performance-test.py も実行可能に

--- performance-test.py
+++ performance-test.py
@@ -10,23 +10,23 @@ MAX_EXPECTED_AMPS = 0.2
 READS = 100
 
 
-ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, log_level=logging.INFO)
 
 
-def init():
+def init(ina):
     ina.configure(ina.RANGE_16V, ina.GAIN_AUTO)
 
 
-def read():
+def read(ina):
     for x in range(0, READS):
         ina.voltage()
 
 
 if __name__ == "__main__":
-    init()
-    start = time.time()
-    read()
-    finish = time.time()
-    elapsed = (finish - start) * 1000000
-    print("Read time (average over %d reads): %d microseconds" %
-          (READS, int(elapsed / READS)))
+    with INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, log_level=logging.INFO) as ina:
+        init(ina)
+        start = time.time()
+        read(ina)
+        finish = time.time()
+        elapsed = (finish - start) * 1000000
+        print("Read time (average over %d reads): %d microseconds" %
+              (READS, int(elapsed / READS)))
